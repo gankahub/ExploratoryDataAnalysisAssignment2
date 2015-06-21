42ebce5597c419f69af02d5e1ff4e439f77de457
Ganka <email.ganka@gmail.com>
2015-06-21 19:30:59 +1000

    Added R code for plot 4.

---
+++ plot4.R
@@ -0,0 +1,16 @@
+#Sourcing data
+#source("SourceData.R")
+
+#Identifying all sources which are coal
+SourceCoal <- SCC[grep("[Cc][Oo][Aa][Ll]",SCC$Short.Name),]
+#Identifying all sources which are comb
+SourceCoalComb <- SourceCoal[grep("[Cc][Oo][Mm][Bb]",SourceCoal$EI.Sector),]
+
+#joining NEI to SCC's which have Source Coal combustion
+NEI_SourceCoalComb <- merge(SourceCoalComb,NEI,by.x="SCC",by.y="SCC",all = FALSE)
+
+#grouping and summarising for easy plotting
+NEI_SourceCoalComb <- group_by(NEI_SourceCoalComb,year)
+NEI_Coal_Sum <- summarise(NEI_SourceCoalComb,Total_Emissions = sum(Emissions))
+
+plot(NEI_Coal_Sum$year, NEI_Coal_Sum$Total_Emissions,xlab = "Year",ylab="Emissions (t)",main = "Emissions from Coal Combustion related Sources")
